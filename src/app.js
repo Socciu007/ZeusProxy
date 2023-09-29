6c77581951d5f8ed270a3e852b077a892ec519ee
ManhTien <nguyenmanhtien.bh@gmail.com>
2023-09-29 15:26:53 +0700

    update

---
+++ src/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const app = express();
+const packagePrices = require("./resources/package-prices.json");
+const questions = require("./resources/faq.json");
+const port = 8080;
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+
+// app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+
+// app.set('views', path.join(__dirname, 'views'));
+
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "/assets")));
+// app.use('/mycdn', express.static(path.join(__dirname, 'node_modules/bootstrap')));
+app.set("view engine", "ejs");
+
+// Dummy users
+// var users = [
+//   { name: 'tobi', email: 'tobi@learnboost.com' },
+//   { name: 'loki', email: 'loki@learnboost.com' },
+//   { name: 'jane', email: 'jane@learnboost.com' }
+// ];
+
+// app.get('/', function(req, res){
+//   res.render('users', {
+//     users: users,
+//     title: "EJS example",
+//     header: "Some users"
+//   });
+// });
+
+app.get("/", async (req, res) => {
+  let packages = packagePrices;
+  // const response = await axios.get("https://api.zeusproxy.com/rates/home");
+  // if (response && response.data && response.data.success) {
+  //   packages = response.data.result;
+  // }
+  res.render(path.join(__dirname, "views/index.ejs"), {
+    packages,
+    questions,
+  });
+});
+app.get("/term-of-services", (req, res) => {
+  res.render(path.join(__dirname, "views/term-of-services.ejs"));
+});
+app.get("/privacy-policy", (req, res) => {
+  res.render(path.join(__dirname, "views/privacy-policy.ejs"));
+});
+app.get("/refund-policy", (req, res) => {
+  res.render(path.join(__dirname, "views/refund-policy.ejs"));
+});
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    telegram: "https://t.me/zeusproxychannel",
+    message: "https://www.facebook.com/messages/t/106013849206579",
+    facebook: "https://www.facebook.com/zeusproxyglobal",
+    skype: "https://join.skype.com/invite/vX2UUFGxPiwu",
+    email: "zeusproxyglobal@gmail.com",
+    youtube: "https://www.youtube.com/@zeusproxy",
+    twister: "https://twitter.com/ZeusProxyGlobal",
+    whatsapp: "https://t.me/zeusproxychannel",
+    dashboard: "https://socks.proxysale.network",
+    policy: "https://docs.google.com/document/d/1WOJx3vPr9puuZbXJBttmUGM-iOnc8re83NXR71NCmHE/edit?usp=sharing",
+    refund:"https://docs.google.com/document/d/1B8QP9PJSGm2allmv0ugAukvHSMJgnqVqdYLtcJwj97g/edit?usp=sharing", 
+    termOfService: "https://docs.google.com/document/d/1NOA4TG5b6loFphr6P9Z3Di6m9b1CN1GM9qW0vcF-Tgo/edit?usp=sharing"
+}
